9f48852872cf34b1e739464e12ab0e2ef175d9a8
Greg <gregory.bobrovsky@gmail.com>
2023-01-16 12:29:12 +0100

    Cli ask for server address, exceptions recreaited from server response.

--- calculator_client/cli.py
+++ calculator_client/cli.py
@@ -1,17 +1,36 @@
 from calculator_client import calculate, CalculationError
 
-if __name__ == "__main__":
+
+def main():
     try:
+        server_address = _ask_for_server_address()
         while True:
-            exp_str = input("Give me some simple equation and press enter,"
-                            " only integers allowed: \n")  # Initial prompt for the user
+            expression = _ask_for_expression()
 
             try:
-                result = calculate(exp_str)
+                result = calculate(expression, server_address)
+
             except CalculationError as e:
-                print("Calculation error: " + str(e))
+                print(str(e), "\n")
+
             else:
                 print(result, "\n")
 
     except KeyboardInterrupt:
         print("\rBye")
+
+
+def _ask_for_server_address():
+    server_address = input("Provide calculation server address:\n")
+    print('')
+    return server_address
+
+
+def _ask_for_expression():
+    expression = input("Provide simple math expression and press enter, "
+                       "only integers allowed:\n")
+    return expression
+
+
+if __name__ == "__main__":
+    main()
